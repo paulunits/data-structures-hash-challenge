1c639bcd2cd43b00d5873830218168a8d00bf5d6
paulunits <obsolete.units@gmail.com>
2016-08-22 17:19:19 -0700

    Add hash class

--- hash.rb
+++ hash.rb
@@ -1,3 +1,28 @@
 class Hash
 
+	attr_reader :key
+
+	def initialize
+		@hash = {}
+	end
+	
+	def set(key, value)
+		@hash[key] = value
+	end	
+
+	def get(key)
+		@hash.fetch(key)
+	end	
+
+	def has_key?(key)
+		@hash.include?(key)
+	end
+	
+	def remove(key)
+		@hash.delete(key)
+	end	
+
+	def iterate
+		@hash.each {|key, value| puts "#{key} => #{value}"}
+	end	
 end
